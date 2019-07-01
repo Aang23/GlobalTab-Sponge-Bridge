e8c19a49bff34dbcbf61f82768108d1eab370f4c
Draycia <lonelyyordle@gmail.com>
2019-07-01 11:36:27 -0700

    Fix plugin message sending

--- src/main/java/com/aang23/globaltabspigotbridge/GlobalTabSpigotBridge.java
+++ src/main/java/com/aang23/globaltabspigotbridge/GlobalTabSpigotBridge.java
@@ -5,9 +5,7 @@ import com.google.common.io.ByteStreams;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,7 +21,7 @@ public class GlobalTabSpigotBridge extends JavaPlugin implements Listener {
             economy = rsp.getProvider();
         }
 
-        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "bungeecord:globaltab");
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "globaltab:sync");
 
         Bukkit.getPluginManager().registerEvents(this, this);
 
@@ -32,17 +30,6 @@ public class GlobalTabSpigotBridge extends JavaPlugin implements Listener {
                 sendBalanceToVelocity(online);
             }
         }, 20, 20);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
-            if (player.isOnline()) {
-                sendBalanceToVelocity(player);
-            }
-        }, 3);
     }
 
     private double getPlayerBalance(Player player) {
@@ -55,6 +42,6 @@ public class GlobalTabSpigotBridge extends JavaPlugin implements Listener {
         out.writeUTF("Balance");
         out.writeUTF(player.getName() + ":" + getPlayerBalance(player));
 
-        player.sendPluginMessage(this, "bungeecord:globaltab", out.toByteArray());
+        player.sendPluginMessage(this, "globaltab:sync", out.toByteArray());
     }
 }
